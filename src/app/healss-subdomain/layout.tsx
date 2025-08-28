b033c387b2be5e39a338f9ff65676c23dc32da60
Kryloss <mr.repar07@gmail.com>
2025-08-27 21:28:01 -0400

    Update

--- src/app/healss-subdomain/layout.tsx
+++ src/app/healss-subdomain/layout.tsx
@@ -1,6 +1,5 @@
 import { AuthProvider } from "@/lib/contexts/AuthContext"
 import { HealssNav } from "./components/healss-nav"
-import "../globals.css"
 
 export default function HealssLayout({
     children,
@@ -8,13 +7,9 @@ export default function HealssLayout({
     children: React.ReactNode
 }) {
     return (
-        <html lang="en">
-            <body className="bg-[#0B0C0D] text-[#FBF7FA] antialiased">
-                <AuthProvider>
-                    <HealssNav />
-                    {children}
-                </AuthProvider>
-            </body>
-        </html>
+        <AuthProvider>
+            <HealssNav />
+            {children}
+        </AuthProvider>
     )
 }
